bb2dbe1d9d7151307bfecf71794f9e3cc89058b7
albertofwb <albertwb951@gmail.com>
2016-04-29 19:50:53 +0800

    working on get specific files

---
+++ repetition_rate/config.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+config_set = {
+    "TOP_DIR":   "/home",
+    "USER_HOME": [
+        "3130931001",
+        "3130931002"
+    ],
+    "COMPARING_FILES": [
+        "/homework1/multi.c"
+    ]
+}
+
+#print config_set.values()
